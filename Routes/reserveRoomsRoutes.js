534a806d8c3af4b946253bdd50305d4301951b22
amanda1686 <amanda160986@gmail.com>
2024-06-06 18:27:23 +0200

    fix:change updatereservation route

--- Routes/reserveRoomsRoutes.js
+++ Routes/reserveRoomsRoutes.js
@@ -7,7 +7,7 @@ router.post('/create', createReservation);
 router.delete('/deletereservations/:reservationId', verifyToken, deleteReservation);
 router.get('/getreservations/:id?', getAllReservations);
 router.get('/getreservations',verifyToken,  getAllReservations);
-router.put('/update-revervations/:reservationId', verifyToken, updateReservationById);
+router.put('/update-reservations/:reservationId', verifyToken, updateReservationById);
 router.put('/close-reservation/:reservationId', closeReservation);
 router.get('/getTotalReservations', verifyToken, getTotalReservations);
 
